24780fd10ea1b84f7492da8eabb47ef6b874dc9a
Peito1102 <renzovasquezch@hotmail.com>
2025-05-31 01:00:14 -0500

    parte 1,2 y 3

---
+++ src/main/java/com/vasquez/curso/springboot/app/springboot_crud/services/ProductServiceImpl.java
@@ -0,0 +1,47 @@
+package com.vasquez.curso.springboot.app.springboot_crud.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.vasquez.curso.springboot.app.springboot_crud.entities.Product;
+import com.vasquez.curso.springboot.app.springboot_crud.repositories.ProductRepository;
+
+@Service
+public class ProductServiceImpl implements ProductService {
+
+    @Autowired
+    ProductRepository repository;
+
+    @Override
+    @Transactional(readOnly = true)
+    public List<Product> findAll() {
+        return (List<Product>) repository.findAll();
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public Optional<Product> findById(Long id) {
+        return repository.findById(id);
+    }
+
+    @Override
+    @Transactional()
+    public Product save(Product product) {
+        return repository.save(product);
+    }
+
+    @Override
+    @Transactional()
+    public Optional<Product> delete(Product product) {
+        Optional<Product> productDb = repository.findById(product.getId());
+        productDb.ifPresent(p -> {
+            repository.delete(product);
+        });
+        return productDb;
+    }
+
+}
