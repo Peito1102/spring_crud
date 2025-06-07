6a47d00e2192496cec95b860bdd24ca0c7f8ba6d
Peito1102 <renzovasquezch@hotmail.com>
2025-06-07 03:55:09 -0500

    parte 1,2 y 3 (jwt 13)

---
+++ src/main/java/com/vasquez/curso/springboot/app/springboot_crud/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package com.vasquez.curso.springboot.app.springboot_crud.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.vasquez.curso.springboot.app.springboot_crud.entities.User;
+
+public interface UserRepository extends CrudRepository<User, Long> {
+
+}
