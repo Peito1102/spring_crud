cb6e62760e441e3fc18ff7afd1f3b882ccfb847e
Peito1102 <renzovasquezch@hotmail.com>
2025-06-17 17:28:39 -0500

    parte 12 (jwt 13)

--- src/main/java/com/vasquez/curso/springboot/app/springboot_crud/repositories/UserRepository.java
+++ src/main/java/com/vasquez/curso/springboot/app/springboot_crud/repositories/UserRepository.java
@@ -1,9 +1,12 @@
 package com.vasquez.curso.springboot.app.springboot_crud.repositories;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.vasquez.curso.springboot.app.springboot_crud.entities.User;
 
 public interface UserRepository extends JpaRepository<User, Long> {
     boolean existsByUsername(String username);
+    Optional<User> findByUsername(String username);
 }
